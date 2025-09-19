56f8d776d0ce50010f7f24e3dd181e9928f8acd9
suvarna2005harshi <suvarnaharshish1908@gmail.com>
2025-09-19 18:15:03 +0530

    Initial commit of MyFirstProgram

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,12 @@
+// This is my first java program
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.printf("Hi Welcome to java basics learning....");
+    }
+}
+//public=access modifier
+//static =key word
+//void=written type
+//main=method name
+//string=class
+//args=array of string object
