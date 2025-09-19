91b07a8072c819fe6ecbe44d4f873aa52cf91b92
suvarna2005harshi <suvarnaharshish1908@gmail.com>
2025-09-19 18:48:30 +0530

    Initial commit of DataTypes.java

---
+++ src/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num1=123456789;
+        long  num2=1238765432L;
+        double num3=4.5;
+        short num4=56;
+        float num5=8.8376456738f;
+        boolean num6=true;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+
+    }
+}
+/*
+output:
+123456789
+1238765432
+4.5
+56
+8.837646
+true
+ */
