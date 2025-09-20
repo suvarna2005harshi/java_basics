037c73b25038af9368d23ae8483a03ec4252f8ec
suvarna2005harshi <suvarnaharshish1908@gmail.com>
2025-09-20 23:23:47 +0530

    addition.java

---
+++ src/addition.java
@@ -0,0 +1,8 @@
+public class addition {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("the sum is:" + sum);
+    }
+}
