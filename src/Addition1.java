b3c4151c3b193e612032874963f863ef14b4ba5e
Suvarna PN <suvarnaharshi1908@gmail.com>
2025-09-25 13:51:43 +0530

    Addition1.java

---
+++ src/Addition1.java
@@ -0,0 +1,21 @@
+public class Addition1 {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int number1 = 10;
+        int number2 = 5;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the result to the console
+        System.out.println("The first number is: " + number1);
+        System.out.println("The second number is: " + number2);
+        System.out.println("The sum of the two numbers is: " + sum);
+    }
+}
+/*
+output:
+The first number is: 10
+The second number is: 5
+The sum of the two numbers is: 15
+ */
